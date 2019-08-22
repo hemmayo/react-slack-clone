c7c42247e694fe096f9a7e337197f69ad721aea1
Emmanuel Popoola <olaayo10@gmail.com>
2019-08-22 16:25:48 +0100

    Organized code and created routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Root = () => (
+  <Router>
+    <Route path="/" exact component={App} />
+    <Route path="/login" exact component={Login} />
+    <Route path="/register" exact component={Register} />
+  </Router>
+);
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
